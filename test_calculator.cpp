0c8f13710e441d0526d05260d8cd0001754221d1
subscriberooizihao <subscriberooizihao@ipk.edu.my>
2025-12-29 09:15:53 +0800

    test_calculator.cpp

--- test_calculator.cpp
+++ test_calculator.cpp
@@ -2,9 +2,9 @@
 #include "calculator.h"
 
 TEST(CalculatorTest, AddTest) {
-EXPECT_EQ(add(2, 3), 5);
+  EXPECT_EQ(add(2, 3), 5);
 }
 
 TEST(CalculatorTest, SubtractTest) {
-EXPECT_EQ(subtract(5, 3), 2);
+  EXPECT_EQ(subtract(5, 3), 2);
 }
